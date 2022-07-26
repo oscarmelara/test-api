c05cffb0159b11b9b48a609f47c45b81a97f1ae3
oscar177 <oscar@propilatam.com>
2022-07-26 16:44:58 -0600

    :sparkles: Agregar variables de entorno

---
+++ README.md
@@ -0,0 +1,11 @@
+# API TEST
+
+## Install the dependencies
+```bash
+yarn
+# or
+npm install
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+npm run dev
--- src/db.ts
+++ src/db.ts
@@ -4,11 +4,11 @@ import { User } from './entities/User'
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'password',
-    port: 5432,
-    database: 'admincardb',
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_HOST,
+    password: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT) || 3306,
+    database: process.env.DATABASE_HOST,
     entities: [Car, User],
     logging: true,
     synchronize: true
