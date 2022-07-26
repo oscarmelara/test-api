a5bbea76d9943f34c1334c4fa999a792266a3e19
oscar177 <oscar@propilatam.com>
2022-07-26 17:03:28 -0600

    :bug: Fixeo de variables de entorno

--- src/db.ts
+++ src/db.ts
@@ -5,10 +5,10 @@ import { User } from './entities/User'
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: process.env.DATABASE_HOST,
-    username: process.env.DATABASE_HOST,
-    password: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     port: Number(process.env.DATABASE_PORT) || 3306,
-    database: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
     entities: [Car, User],
     logging: true,
     synchronize: true
